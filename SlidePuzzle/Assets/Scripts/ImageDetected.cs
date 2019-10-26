11c3e05468d802da6f30c38dba19d6481982bd2f
YiyinEllenGu <yiyingu0512@gmail.com>
2019-10-26 01:05:04 -0400

    initial commit

---
+++ SlidePuzzle/Assets/Scripts/ImageDetected.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageDetected : MonoBehaviour
+{
+    bool detected = false;
+    public GameObject tileMap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!detected && (GameObject.FindGameObjectWithTag("empty") != null))
+        {
+            Instantiate(tileMap, new Vector3(0, 0, 0), Quaternion.identity);
+            detected = true;
+        }
+        else if(detected && (GameObject.FindGameObjectWithTag("empty") == null))
+        {
+            Destroy(GameObject.FindGameObjectWithTag("tileMap"));
+            detected = false;
+        }
+    }
+}
